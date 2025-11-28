1710860321f33a32c2bea5782b6767e37a087d16
AlexhHr23 <alex.hernandez24kb@gmail.com>
2025-11-27 21:40:05 -0600

    Uso muts

--- handlers/hadlers.go
+++ handlers/hadlers.go
@@ -3,23 +3,17 @@ package handlers
 import (
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
 )
 
+const (
+	templateDir  = "templates/"
+	templateBase = templateDir + "base.html"
+)
+
 func Index(w http.ResponseWriter, r *http.Request) {
-	tpl, err := template.ParseFiles("templates/base.html", "templates/index.html")
-
-	if err != nil {
-		http.Error(w, "Error al analizar plantillas", http.StatusInternalServerError)
-		return
-	}
-
-	err = tpl.ExecuteTemplate(w, "base", nil)
-
-	if err != nil {
-		http.Error(w, "Error al renderizar la plantilla", http.StatusInternalServerError)
-		return
-	}
+	renderTemplate(w, templateBase, "index.html", nil)
 }
 
 func NewGame(w http.ResponseWriter, r *http.Request) {
@@ -36,4 +30,14 @@ func Play(w http.ResponseWriter, r *http.Request) {
 
 func Aboiut(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Acerca de")
+}
+
+func renderTemplate(w http.ResponseWriter, base, page string, data any) {
+	tpl := template.Must(template.ParseFiles(base, templateDir+page))
+	err := tpl.ExecuteTemplate(w, "base", data)
+	if err != nil {
+		http.Error(w, "Error al analizar plantillas", http.StatusInternalServerError)
+		log.Println(err)
+		return
+	}
 }
