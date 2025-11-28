e73d423cb941399ae4c18ea60404ce6d765d3e18
AlexhHr23 <alex.hernandez24kb@gmail.com>
2025-11-27 21:56:04 -0600

    Configuracion

--- main.go
+++ main.go
@@ -10,6 +10,12 @@ func main() {
 
 	router := http.NewServeMux()
 
+	//Manejador para servir archivos
+	fs := http.FileServer(http.Dir("static"))
+
+	//Ruta para acceder a los archivos estaticos
+	router.Handle("/static/", http.StripPrefix("/static/", fs))
+
 	router.HandleFunc("/", handlers.Index)
 	router.HandleFunc("/new", handlers.NewGame)
 	router.HandleFunc("/game", handlers.Game)
